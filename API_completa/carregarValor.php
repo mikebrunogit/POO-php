d8a271e977001160dc5dd2021430cd1c22a7ae76
Mike Bruno <133905317+mikebrunogit@users.noreply.github.com>
2023-10-31 22:10:36 -0300

    commit - comentário

--- API_completa/carregarValor.php
+++ API_completa/carregarValor.php
@@ -6,20 +6,24 @@ header("Access-Control-Allow-Origin: *", false);
 
 include 'conexao.php';
 
+
+//
 $dados = file_get_contents('php://input');
 
-
-$dados = file_get_contents('php://input');
 $array = json_decode($dados);
-//$recebe_id = $array  ->id;
-$recebe_id = '90';
 
+//recebe o id decodificado presente no array
+$recebe_id = $array  ->id;
+
+//consulta dos dados da tab_valor (lançamentos financeiros)
 $consulta = $conexao->query("SELECT * FROM tab_valor WHERE id_user='$recebe_id' ");
 
-
+//condifciona if: prossegue caso o usário tenha um lançamento
+//condiciona else: caso o usuario não tenha, 0 será enviado e uma mebnsagem será exibida
 if ($consulta->rowCount()==0) {
     $resposta = array('Resp'=> '0'  );
 }else{
+	//exibe as informações do lançamento
     while($exibe=$consulta->fetch(PDO::FETCH_ASSOC)){
         $resposta []= array('id' => $exibe['id'],
         'data' => $exibe['addData'],
