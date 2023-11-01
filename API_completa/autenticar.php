5d094261459496a6051f10640efb38e0f495a16b
Mike Bruno <133905317+mikebrunogit@users.noreply.github.com>
2023-10-31 21:59:02 -0300

    Add files via upload

--- API_completa/autenticar.php
+++ API_completa/autenticar.php
@@ -5,17 +5,25 @@ header('Access-Control-Allow-Origin:*');
 
 include 'conexao.php';
 
+
+//Puxa os dados e decodifica para o restante do código php
 $dados = file_get_contents('php://input');
 $array = json_decode($dados);
 $recebe_email = $array  ->email;
 $recebe_senha = $array  ->senha;
 
 
+
+//faz consulta no banco de dados e aramzena em um atributo
 $consulta = $conexao->query("SELECT * FROM tab_user WHERE email='$recebe_email' AND senha='$recebe_senha'");
 
+
+//condiciobal if: verifica o login, através da consulta de banco de dados anterior alocada no atributo   
 if ($consulta->rowCount()==0) { 
     $resposta = array('Resp'=> '0');
 
+
+// condicional else: Caso o usuário já tenha cadastro, retorna os dados do usuário
 }else {
         $exibe=$consulta->fetch(PDO::FETCH_ASSOC);
         $resposta = array('Resp'=> '1',
